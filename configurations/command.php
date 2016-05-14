4be24cddcf5cf19cb328452fd6a365b063baf968
Jack Wan <hwguyguy@gmail.com>
2016-05-14 12:03:41 +0800

    Update description in command configuration

--- configurations/command.php
+++ configurations/command.php
@@ -2,7 +2,7 @@
 /**
  * Configurations of command.
  *
- * "namespaces": Array of namespaces searched by command service.
+ * "namespaces": Array of namespaces searched by command service, from lower to higher priority.
  * "rules": Rules for searching command.
  */
 return [
