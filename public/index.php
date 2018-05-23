3e1c54df074e3e1725588860e3392d3732509a62
Jack Wan <hwguyguy@gmail.com>
2018-05-23 16:02:01 +0800

    Update cli

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
+
 $container = new Rise\Services\Container();
 $initializer = $container->get('Rise\Services\Initializer');
 $initializer->setProjectRootPath(__DIR__.'/..')->run();
